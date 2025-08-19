461551fd9510c8adccb82304ea8bd54bc7b065c1
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-08-19 07:49:12 -0700

    Bank-App

--- app.js
+++ app.js
@@ -4,69 +4,80 @@ let crtAccDiv = document.getElementById("crtAccDiv")
 let accName   = document.getElementById("accName")
 let accAmount = document.getElementById("accAmount")
 let crtBkAccBtn = document.getElementById("crtBkAccBtn")
+
 let addAccDiv  = document.getElementById("addAccDiv")
+let numbering = 0
 
 class BankAccount {
     
     constructor (owner , balance = 0) {
-        
-        this.AccOwner = owner
+
+        this.AccOwner = owner.toUpperCase()
         this.OwnBalan = balance
+        this.id = numbering
         
-        // Create UI elements
-        this.container = document.createElement("div")
-        this.container.classList.add("accDiv")
-        
-        this.balTxt = document.createElement("p")
-        this.balTxt.innerText = `${this.AccOwner} Current Balance : ${this.OwnBalan}`
-    
-        this.input = document.createElement("input")
-        this.input.type = "number"
-        this.input.placeholder = "Enter Amount"
-    
-        this.deposBtn = document.createElement("button")
-        this.deposBtn.innerText = "Deposit"
-
-        this.withDrawBtn = document.createElement("button")
-        this.withDrawBtn.innerText = "Withdraw"
-
-        // Attached elements in Container
-        this.container.appendChild(this.balTxt)
-        this.container.appendChild(this.input)
-        this.container.appendChild(this.deposBtn)
-        this.container.appendChild(this.withDrawBtn)
-        addAccDiv.appendChild(this.container)
+        // Add Tr Values
+        document.getElementById("tableHeadings").innerHTML =
+        `
+            <th class="border text-center px-5 py-2 bg-[#01916f] text-white">NO.</th>
+            <th class="border text-center px-5 py-2 bg-[#01916f] text-white">ACCOUNT NAME</th>
+            <th class="border text-center px-5 py-2 bg-[#01916f] text-white">BALANCE</th>
+            <th class="border text-center px-5 py-2 bg-[#01916f] text-white">DEPOSIT</th>
+            <th class="border text-center px-5 py-2 bg-[#01916f] text-white">WITHDRAW</th>
+        `
+        let tr = document.createElement("tr")
+        tr.id = `tableData-${this.id}`
+        tr.innerHTML += 
+        `
+        <tr id="tableData-${this.id}">
+            <td class="border text-center px-5 py-1">${this.id}</td>
+            <td class="border text-center px-5 py-1">${this.AccOwner}</td>
+            <td class="border text-center px-5 py-1" id="balancTxt-${this.id}">${this.OwnBalan}</td>
+            <td class="border text-center px-5 py-1">
+                <input type="number" class="border text-center rounded-xl px-1 py-4 w-28 h-5 my-1" placeholder="Amount" id="deposit-${this.id}">
+            </td>
+            <td class="border text-center px-5 py-1">
+                <input type="number" class="border text-center rounded-xl px-1 py-4 w-28 h-5 my-1" placeholder="Amount" id="withdraw-${this.id}">
+            </td>
+        </tr>
+        `
+        document.getElementById("addAccDiv").appendChild(tr)
 
         // Event Listeners
-        this.deposBtn.addEventListener("click" , () => {
-            this.deposit(Number(this.input.value))
-            this.input.value = ""
+        let depInp = document.getElementById(`deposit-${this.id}`);
+        depInp.addEventListener("keydown" , (e) => {
+            if (e.key === "Enter") {
+                let depositInp = document.getElementById(`deposit-${this.id}`)
+                this.deposit(Number(depositInp.value))
+                depositInp.value = ""
+            }
         })
 
-        this.withDrawBtn.addEventListener("click" , () => {
-            this.withDraw(Number(this.input.value))
-            this.input.value = ""
+        let withdraw = document.getElementById(`withdraw-${this.id}`);
+        withdraw.addEventListener("keydown" , (e) => {
+            if (e.key === "Enter") {
+                let withdrawInp = document.getElementById(`withdraw-${this.id}`)
+                this.withDraw(Number(withdrawInp.value))
+                withdrawInp.value = ""
+            }
         })
-        
-    }
 
-    checkBalance() {
-        console.log(`${this.AccOwner} Current Balance : ${this.OwnBalan}`);
     }
 
     deposit(amount) {
         this.OwnBalan += amount
-        this.balTxt.innerText = `${this.AccOwner} Current Balance : ${this.OwnBalan}`
+        document.getElementById(`balancTxt-${this.id}`).innerText = this.OwnBalan
     }
 
     withDraw(amount) {
         this.OwnBalan -= amount
-        this.balTxt.innerText = `${this.AccOwner} Current Balance : ${this.OwnBalan}`
+        document.getElementById(`balancTxt-${this.id}`).innerText = this.OwnBalan
     }
 
 }
 
 crtBkAccBtn.addEventListener("click" , () => {
+    numbering++
     new BankAccount(accName.value , Number(accAmount.value))
     accName.value = ""
     accAmount.value = ""
